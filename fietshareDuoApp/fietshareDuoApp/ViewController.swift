0991041d8921da4f34949de655bcd46775210204
Student <student@ISSDs-MacBook-Pro-7.local>
2019-10-17 23:14:41 +0200

    xCode DuoApp started

---
+++ fietshareDuoApp/fietshareDuoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fietshareDuoApp
+//
+//  Created by Student on 17/10/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
