e5e110c7e38f96e1d7702343bcb21977845f89de
Student <student@ISSDs-MacBook-Pro.local>
2019-11-08 11:14:17 +0100

    Second half of the app completed

---
+++ Fietshare/Fietshare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fietshare
+//
+//  Created by Student on 15/10/2019.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+}
+
